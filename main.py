ff84cab6e3478e3030ff105e286c81e8a891d8a0
BITAN MAJUMDER <108423919+bitanM@users.noreply.github.com>
2024-01-09 12:42:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  2 18:55:25 2023
-
-@author: HP-PC
-"""
 from gensim.models import Doc2Vec
 import data
 import vectors
 import clustering
 from sklearn.metrics import silhouette_score
-#import nltk
-#from nltk.corpus import stopwords
-#import re
-#from striprtf.striprtf import rtf_to_text
-#from gensim.models.doc2vec import TaggedDocument
-#import os
-#import numpy as np
-#from scipy.cluster.hierarchy import linkage
-#import matplotlib.pyplot as plt
-#from sklearn.cluster import DBSCAN
-#import model_file
-
 
 vector_list = []
 cluster_list = []
